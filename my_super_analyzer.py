072cd67f5912983bdfd9c05b8ebe5d580aed53f3
PizzaDeliveryBoy <59928277+PizzaDeliveryBoy@users.noreply.github.com>
2025-03-28 16:18:48 +0500

    Update my_super_analyzer.py

--- my_super_analyzer.py
+++ my_super_analyzer.py
@@ -2,7 +2,7 @@ import argparse
 import time
 import os
 import pandas as pd
-import webbrowser
+import requests
 import re
 
 parser = argparse.ArgumentParser(description='CVE')
@@ -10,14 +10,15 @@ parser.add_argument('--link', type=str, help='--link URL')
 parser.add_argument('--keywords', type=str, help='--keywords "keyword1,keyword2..."')
 parser1 = parser.parse_args()
 
-FILE_URL = parser1.link
-FILE_PATH = os.path.expanduser("~/Downloads/thrlist.xlsx")
+URL = parser1.link
 
-webbrowser.open(FILE_URL)
-time.sleep(5)
+response = requests.get(URL)
+
+with open('thrlist.xlsx', 'wb') as file:
+    file.write(response.content)
 
 pd.set_option('display.max_colwidth', None)
-df = pd.read_excel(FILE_PATH, header = 1)
+df = pd.read_excel('thrlist.xlsx', header = 1)
 
 KEYWORDS_SPLIT = parser1.keywords.split(',')
 search_keywords = '|'.join(map(re.escape, KEYWORDS_SPLIT))
@@ -26,6 +27,10 @@ matches = df['Наименование УБИ'].str.contains(search_keywords, ca
 
 filter = df[matches]
 
-print(filter['Наименование УБИ'].to_string(index=False))
+columns_to_display = ['Идентификатор УБИ', 'Наименование УБИ', 'Нарушение конфиденциальности', 'Нарушение целостности', 'Нарушение доступности']
 
-os.remove(FILE_PATH)
+filtered_columns = [col for col in columns_to_display if col in filter.columns]
+
+print(filter[filtered_columns].to_string(index=False))
+
+os.remove('thrlist.xlsx')
